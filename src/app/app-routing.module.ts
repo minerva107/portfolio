3c923f5e4e4d0192d72561398386ec3a894d0eaa
Simone Fernandes <55145901+minerva107@users.noreply.github.com>
2020-03-14 18:03:40 +0000

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { EmailComponent } from './email/email.component';
 import { AboutmeComponent } from './aboutme/aboutme.component';
+import { PaginaprincipalComponent } from './paginaprincipal/paginaprincipal.component';
 
 const routes: Routes = [
 
@@ -10,6 +11,7 @@ const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'email',   component: EmailComponent },
   {path: 'aboutme',   component: AboutmeComponent },
+  {path: 'paginaprincipal',   component: PaginaprincipalComponent }
 
 ];
 
